956493b739b22b9bf57b42351bdb973497440022
sing0017 <sing0017@edu.easj.dk>
2017-09-27 08:50:10 +0200

    changeallinone

---
+++ Exc4student/Exc4student/IService1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Exc4student
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+
+        [OperationContract]
+        void Addstudent(Student st);
+
+        [OperationContract]
+        Student findstudent(string st);
+
+        [OperationContract]
+        void removestudent(int st);
+
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Student
+    {
+        [DataMember]
+        public int index { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string No { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1}", Name, No);
+        }
+    }
+}
---
+++ Exc4student/Exc4student/Service1.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Exc4student
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public static List<Student> stt = new List<Student>();
+
+        public void Addstudent(Student st)
+        {
+            stt.Add(st);
+        }
+
+        public Student findstudent(string st)
+        {
+            return stt.Find(Student => Equals(st, Student.Name));
+
+        }
+
+        public void removestudent(int st)
+        {
+            stt.RemoveAt(st);
+
+        }
+
+
+    }
+}
